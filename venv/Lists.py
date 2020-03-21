560c20562c11dc1c4eb1d56128726e888a505570
arwiew <pocht1991@yandex.ru>
2020-03-21 14:56:13 +0300

    Задачи на листы

---
+++ venv/Lists.py
@@ -0,0 +1,20 @@
+list = [9, 4, 5, 2, 3]
+newList = []
+for i in list:
+    if (i % 2) != 0:
+       newList.append(i)
+    else: continue
+print(newList)
+
+list = [5, 1, 3, 6, 1]
+newList = []
+check = list[0]
+for i in list:
+    if i > check:
+       print(i, check)
+       newList.append(i)
+       check = i
+    else:
+        check = i
+        continue
+print(newList)
