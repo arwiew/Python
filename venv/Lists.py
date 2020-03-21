217745104187a96e5897e09838a8286f4b4f88ff
arwiew <pocht1991@yandex.ru>
2020-03-21 20:47:32 +0300

    Задачи на листы

--- venv/Lists.py
+++ venv/Lists.py
@@ -24,3 +24,15 @@ list.sort()
 print(list)
 
 
+# Обработка списка по индексу
+elist = [90, 45, 3, 43]
+even_list = []
+for el in range(0,len(elist)):
+    if el % 2 == 0:
+        print(elist[el])
+        even_list.append(elist[el])
+    else:
+        continue
+print(even_list)
+
+
