79e3d41a8a690dc9be590183aa9f7ad8fb8690cf
arwiew <pocht1991@yandex.ru>
2020-03-22 13:53:15 +0300

    Задачи на листы

---
+++ venv/Dict.py
@@ -0,0 +1,25 @@
+dict = {'Hello' : 'Hi', 'Bye' : 'Goodbye', 'List' : 'Array'}
+
+# Вывод значения по ключу
+say = input()
+print(dict.get(say))
+# Вывод ключа по значению
+for key, value in dict.items():
+        if say == value:
+            print(key)
+
+
+list = ['abc', 'bcd', 'abc', 'abd', 'abd', 'dcd', 'abc']
+list.sort()
+exlist = []
+check = ''
+for k in list:
+    if k != check:
+        exlist.append(k)
+        check = k
+    else: None
+print(exlist)
+even_list = []
+for i in exlist:
+    even_list.append(list.count(i))
+print(even_list)
